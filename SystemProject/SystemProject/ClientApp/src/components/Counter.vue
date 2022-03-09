c339b900c547f4ee49dd8de41e8ef16a9fcaa195
developer.sokkrey@gmail.com <developer.sokkrey@gmail.com>
2022-03-09 15:28:13 +0700

    add tailwindcss

--- SystemProject/SystemProject/ClientApp/src/components/Counter.vue
+++ SystemProject/SystemProject/ClientApp/src/components/Counter.vue
@@ -1,11 +1,14 @@
 <template>
-    <h1>Counter</h1>
+    <h1 class="">Counter</h1>
 
-    <p>This is a simple example of an Vue component.</p>
+    <p class="text-slate-5000">This is a simple example of an Vue component.</p>
 
-    <p aria-live="polite">Current count: <strong>{{ currentCount }}</strong></p>
-
-    <button class="btn btn-primary" @click="incrementCounter">Increment</button>
+    <p aria-live="polite" class="text-blue-400">Current count: <strong>{{ currentCount }}</strong></p>
+    <span class="icon text-pink-400"><i class="mdi mdi-desktop-mac"></i></span>
+    <h1 class="text-3xl font-bold underline text-blue-400">
+        Hello world!
+    </h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="incrementCounter">Increment</button>
 </template>
 
 
