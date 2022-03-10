ce75a4118c673696228c4e5ffec0a4cea50577c9
developer.sokkrey@gmail.com <developer.sokkrey@gmail.com>
2022-03-10 15:58:28 +0700

    Admin vue

--- SystemProject/SystemProject/ClientApp/src/router/index.js
+++ SystemProject/SystemProject/ClientApp/src/router/index.js
@@ -1,29 +1,96 @@
-﻿import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/components/Home.vue";
-import Counter from "@/components/Counter.vue";
-import FetchData from "@/components/FetchData.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
+  {
+    meta: {
+      title: 'Select style',
+      fullScreen: true
     },
-    {
-        path: "/Counter",
-        name: "Counter",
-        component: Counter,
+    path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    // Document title tag
+    // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
+    meta: {
+      title: 'Dashboard'
     },
-    {
-        path: "/FetchData",
-        name: "FetchData",
-        component: FetchData,
-    }
-];
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Home
+  },
+  {
+    meta: {
+      title: 'Tables'
+    },
+    path: '/tables',
+    name: 'tables',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "tables" */ '@/views/Tables.vue')
+  },
+  {
+    meta: {
+      title: 'Forms'
+    },
+    path: '/forms',
+    name: 'forms',
+    component: () => import(/* webpackChunkName: "forms" */ '@/views/Forms.vue')
+  },
+  {
+    meta: {
+      title: 'Profile'
+    },
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "profile" */ '@/views/Profile.vue')
+  },
+  {
+    meta: {
+      title: 'Ui'
+    },
+    path: '/ui',
+    name: 'ui',
+    component: () => import(/* webpackChunkName: "ui" */ '@/views/Ui.vue')
+  },
+  {
+    meta: {
+      title: 'Responsive layout'
+    },
+    path: '/responsive',
+    name: 'responsive',
+    component: () => import(/* webpackChunkName: "responsive" */ '@/views/Responsive.vue')
+  },
+  {
+    meta: {
+      title: 'Login',
+      fullScreen: true
+    },
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+  },
+  {
+    meta: {
+      title: 'Error',
+      fullScreen: true
+    },
+    path: '/error',
+    name: 'error',
+    component: () => import(/* webpackChunkName: "error" */ '@/views/Error.vue')
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || { top: 0 }
+  }
+})
 
-export default router;
+export default router
